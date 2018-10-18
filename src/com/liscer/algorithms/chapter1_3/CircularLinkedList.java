863219f917c7e18bf7e3fd52be3568e0d9b3ae98
liscer <1525159595@qq.com>
2018-10-18 20:29:22 +0800

    循环链表增加指定位置add和delete

--- src/com/liscer/algorithms/chapter1_3/CircularLinkedList.java
+++ src/com/liscer/algorithms/chapter1_3/CircularLinkedList.java
@@ -3,57 +3,73 @@ package com.liscer.algorithms.chapter1_3;
 import java.util.NoSuchElementException;
 
 public class CircularLinkedList<Item> {
-	
+
 	private Node head;
 	private int N;
-	
-	class Node{
+
+	class Node {
 		Item item;
 		Node next;
-		public Node(Item item){
+
+		public Node(Item item) {
 			this.item = item;
 		}
 	}
-	public CircularLinkedList(){
+
+	public CircularLinkedList() {
 		head = new Node(null);
-		head.next = head;//没有节点的时候head的下一个是自己
+		head.next = head;// 没有节点的时候head的下一个是自己
 		N = 0;
 	}
-	
-	public boolean isEmpty(){
-		return head == null;
+
+	public boolean isEmpty() {
+		return N == 0;
 	}
-	
-	public int size(){
+
+	public int size() {
 		return N;
 	}
-	
-	public void add(Item item){
+
+	public void add(Item item) {
 		Node newNode = new Node(item);
-		if (head.next == head) {
-			head.next = newNode;
-			newNode.next = head;
-			N++;
-		}else {
-			Node current = head;
-			while (current.next!= head) {
+		Node current = head;
+		while (current.next != head) {
+			current = current.next;
+		}
+		current.next = newNode;
+		newNode.next = head;
+		N++;
+	}
+
+	public void add(Item item, int index) {
+		if (index < 0 || index > N - 1) {
+			throw new IndexOutOfBoundsException();
+		} else {
+			Node current = head.next ;
+			int counter = 0;
+			while (current != head) {// 游标先去,移动在后,在while里只能操作前一个
+				if (index == counter++) {
+					Node newNode = new Node(item);
+					newNode.next = current.next;
+					current.next = newNode;
+					N++;
+					return;
+				}
 				current = current.next;
 			}
-			current.next = newNode;
-			newNode.next = head;
-			N++;
 		}
 	}
-	
-	public void remove(){
+
+	public void remove() {
 		if (N == 0) {
 			throw new NoSuchElementException();
 		}
 		if (N == 1) {
 			head.next = head;
-		}else {
-			Node current= head ;
-			while (current.next.next!= head) {
+			N--;
+		} else {
+			Node current = head;
+			while (current.next.next != head) {
 				current = current.next;
 			}
 			current.next = head;
@@ -61,11 +77,54 @@ public class CircularLinkedList<Item> {
 		}
 	}
 	
+	public void delete(int index){
+		if (index < 0 || index>N-1) {
+			throw new IndexOutOfBoundsException();
+		}else if (index == N-1) {
+			remove();
+		}else {
+			int counter = 0;
+			for(Node current = head;current != null;current = current.next){
+				if (index == counter++) {
+					current.next = current.next.next;
+					N--;
+					return;
+				}
+			}
+		}
+	}
+
+	public Item get(int index) {
+		if (index < 0 || index > N - 1) {
+			throw new IndexOutOfBoundsException();
+		}
+		int counter = 0;
+		Node current = head.next;
+		while (current != head) {
+			if (index == counter++) {
+				return current.item;
+			}
+			current = current.next;
+		}
+		return null;
+	}
+
+	public boolean find(CircularLinkedList<Item> linked, Item key) {
+		Node current = linked.head;
+		while (current.next != head) {
+			if (key.equals(current.item)) {
+				return true;
+			}
+			current = current.next;
+		}
+		return false;
+	}
+
 	@Override
 	public String toString() {
 		StringBuilder sbBuilder = new StringBuilder();
 		Node current = head;
-		while(current.next != head){
+		while (current.next != head) {
 			sbBuilder.append(current.next.item);
 			sbBuilder.append(" ");
 			current = current.next;
@@ -76,12 +135,22 @@ public class CircularLinkedList<Item> {
 	public static void main(String[] args) {
 		CircularLinkedList<String> circularLined = new CircularLinkedList<>();
 		circularLined.add("aaa");
-		circularLined.add("aaa");
-		circularLined.add("aaa");
-		circularLined.add("aaa");
-		circularLined.remove();
-		System.out.println(circularLined.size());
+		circularLined.add("bbb");
+		// circularLined.add("aaa");
+		// circularLined.add("aaa");
+		// circularLined.remove();
+		// circularLined.remove();
+		// circularLined.remove();
+		circularLined.add("ccc");
+		// circularLined.add("bbb");
+		//System.out.println(circularLined.size());
+		//circularLined.add("ddd",1);
+		circularLined .delete(0);
 		System.out.println(circularLined.toString());
+		System.out.println(circularLined.isEmpty());
+		System.out.println(circularLined.size());
+		//System.out.println(circularLined.get(2));
+		//System.out.println(circularLined.find(circularLined, "aaa"));
 	}
 
 }
