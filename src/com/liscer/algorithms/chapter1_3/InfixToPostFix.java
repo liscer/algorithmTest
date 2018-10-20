eba2e7beece74f1e07d207ef6e9a320c54fce1f5
liscer <1525159595@qq.com>
2018-10-20 16:15:16 +0800

    啥玩意呀

---
+++ src/com/liscer/algorithms/chapter1_3/InfixToPostFix.java
@@ -0,0 +1,23 @@
+package com.liscer.algorithms.chapter1_3;
+
+import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class InfixToPostFix {
+
+	public static void main(String[] args) {
+		 Stack<String> stack = new Stack<String>();
+	        while (!StdIn.isEmpty()) {
+	            String s = StdIn.readString();
+	            if      (s.equals("+")) stack.push(s);
+	            else if (s.equals("*")) stack.push(s);
+	            else if (s.equals(")")) StdOut.print(stack.pop() + " ");
+	            else if (s.equals("(")) StdOut.print("");
+	            else                    StdOut.print(s + " ");
+	        }
+	        StdOut.println();
+
+	}
+
+}
